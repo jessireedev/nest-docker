2bd73cbca081e2d4ced8b5ec128ea41c7d884377
Jessiree De Vera <jessireedevera@gmail.com>
2025-01-28 22:48:42 +0800

    feat: Added initial swagger

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const config = new DocumentBuilder()
-    .setTitle('nest-docker')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .build();
+  const config = new DocumentBuilder().setTitle('nest-docker').setVersion('1.0').build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
